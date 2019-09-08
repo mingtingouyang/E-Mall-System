ea72a5da24c3ca0801987cccf9f6e7a8a627a395
mingtingouyang <carlosouyang@outlook.com>
2019-09-08 22:49:57 +0800

    将项目改为 spring boot 构建

---
+++ ego-order/src/main/java/org/oza/ego/order/EgoOrderApplication.java
@@ -0,0 +1,13 @@
+package org.oza.ego.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgoOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgoOrderApplication.class, args);
+    }
+
+}
---
+++ ego-portal/src/main/java/org/oza/ego/portal/EgoPortalApplication.java
@@ -0,0 +1,13 @@
+package org.oza.ego.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgoPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgoPortalApplication.class, args);
+    }
+
+}
